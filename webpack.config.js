a2e604d2422e000afeffa1514453584557219493
Work <roniel-work@pop-os.localdomain>
2019-11-07 12:09:53 -0400

    Implement base template and headers using semantic

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,66 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const extractCSS = new ExtractTextPlugin('site.css');
+const extractCSS = new ExtractTextPlugin("site.css");
 
 module.exports = {
   entry: {
-      "dummy.js": "./src/js/dummy.js",
-      "site.js": "./src/js/site.js",
-      //"site.css": "./src/css/site.less",
-      "vendor.js": ['knockout', 'jquery'],
+    "dummy.js": "./src/js/dummy.js",
+    "site.js": "./src/js/site.js",
+    "site.css": "./src/css/site.less",
+    "vendor.js": ["knockout", "jquery"]
   },
   module: {
     rules: [
       {
         test: /\.less$/,
         loader: extractCSS.extract({
-          use: [
-            {loader: "css-loader?minimize"},
-            {loader: "less-loader"},
-          ],
+          use: [{ loader: "css-loader?minimize" }, { loader: "less-loader" }]
         })
       },
       {
         test: /\.jpe?g$|\.gif$|\.png$|\.ttf$|\.eot$|\.svg$/,
-        use: 'file-loader?name=[name].[ext]?[hash]'
+        use: "file-loader?name=[name].[ext]?[hash]"
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=1000&mimetype=application/fontwoff'
-      },
+        loader: "url-loader?limit=1000&mimetype=application/fontwoff"
+      }
     ]
+  },
+  devServer: {
+    hot: true
   },
   resolve: {
     alias: {
       // This is required to override the semanticui theme.config
-      '../../theme.config': path.join(
-        __dirname,
-        'src/theme/theme.config'
-      ),
-      '../../src/theme/site': path.join(
-        __dirname,
-        'src/theme/site'
-      ),
+      "../../theme.config": path.join(__dirname, "src/theme/theme.config"),
+      "../../src/theme/site": path.join(__dirname, "src/theme/site")
     },
-    extensions: ['.less', '.js', '.json'],
+    extensions: [".less", ".js", ".json"]
   },
   plugins: [
     extractCSS,
     new webpack.optimize.CommonsChunkPlugin({
-      names: ['vendor.js'],
-      minChunks: 1,
+      names: ["vendor.js"],
+      minChunks: 1
     }),
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-    }),
+      $: "jquery",
+      jQuery: "jquery"
+    })
   ],
   output: {
-    filename: '[name]',
-    path: path.join(__dirname, 'readthedocsext', 'theme', 'static', 'readthedocsext', 'theme')
-  },
+    filename: "[name]",
+    path: path.join(
+      __dirname,
+      "readthedocsext",
+      "theme",
+      "static",
+      "readthedocsext",
+      "theme"
+    ),
+    publicPath: "/static/"
+  }
 };
