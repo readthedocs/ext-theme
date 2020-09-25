7a8dc57fdc61e5df27cac1bc382b1d11cfe2c1a1
Anthony Johnson <aj@ohess.org>
2020-09-25 10:45:58 -0600

    Fix minification issue with class names

--- src/js/application/views.js
+++ src/js/application/views.js
@@ -6,25 +6,30 @@ import * as build from "../build";
 import * as docs from "../docs";
 import { MessageView } from "../core/views";
 
-const views = [
-  build.detail.BuildDetailView,
-  project.ProjectListView,
-  project.ProjectVersionListView,
-  project.create.ProjectCreateView,
-  project.admin.ProjectSearchAnalyticsView,
-  project.admin.ProjectTrafficAnalyticsView,
-  project.admin.ProjectRedirectView,
-  project.admin.ProjectAutomationRuleView,
-  docs.EmbedTopicsView,
-  MessageView,
-];
+/* Fun fact, we can't rely on dynamic naming here, like using
+ * ``ProjectListView.constructor.name``, as minification renames the class
+ */
+const views = {
+  BuildDetailView: build.detail.BuildDetailView,
+  ProjectListView: project.ProjectListView,
+  ProjectVersionListView: project.ProjectVersionListView,
+  ProjectCreateView: project.create.ProjectCreateView,
+  ProjectSearchAnalyticsView: project.admin.ProjectSearchAnalyticsView,
+  ProjectTrafficAnalyticsView: project.admin.ProjectTrafficAnalyticsView,
+  ProjectRedirectView: project.admin.ProjectRedirectView,
+  ProjectAutomationRuleView: project.admin.ProjectAutomationRuleView,
+  EmbedTopicsView: docs.EmbedTopicsView,
+  MessageView: MessageView,
+};
 
 export class ApplicationView {
   constructor() {
     console.debug("Configuring application view subviews");
-    for (const view_class of views) {
-      this[view_class.name] = (params) => {
-        console.debug("Loading view with parameters:", view_class.name, params);
+    for (const [view_name, view_class] of Object.entries(views)) {
+      this[view_name] = (params) => {
+        console.debug("Loading view with parameters:", view_name, params);
+        // ignoreDependencies is needed here or the context used by the subview
+        // is incorrect
         return ko.ignoreDependencies(() => {
           return new view_class(params);
         }, this);
