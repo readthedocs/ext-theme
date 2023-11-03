1a65b35ae67c8d9934696cc52ab63282b6e950dd
Anthony <aj@ohess.org>
2023-11-02 18:31:36 -0700

    Add organization team and team member view templates (#226)
    * Add organization team and team member view templates
    
    Refs #222
    
    * Fix memberlist placeholder paragraph alignment

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -1,4 +1,5 @@
 import * as popupcards from "./popupcards";
 import * as header from "./header";
+import * as filter from "./filter";
 
-export { popupcards, header };
+export { popupcards, header, filter };
