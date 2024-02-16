1da5a1e86641e0f94d374b1a9e14566ef3554d97
Anthony Johnson <aj@ohess.org>
2024-02-16 11:21:00 -0800

    Add template and model view changes for organization authorization settings
    This relies on https://github.com/readthedocs/readthedocs-corporate/pull/1730
    
    This drops the very custom form for a more native form, but retains some
    of the styles of the authorization provider list. This is now just a
    informational block describing the providers.
    
    This also adds the KO view logic back to the view, so that we can
    conditionally show/require the domain field.
    
    Also raised in the PR above, a warning block is added when the provider
    value is changing, so there is some information that
    authentication/authorization might fail.

--- src/js/organization/index.js
+++ src/js/organization/index.js
@@ -58,3 +58,40 @@ export class OrganizationListView {
   }
 }
 Registry.add_view(OrganizationListView);
+
+/**
+ * Organization authorization settings
+ */
+export class OrganizationSettingsAuthorizationView {
+  static view_name = "OrganizationSettingsAuthorizationView";
+
+  constructor() {
+    this.provider_original = undefined;
+    this.provider = ko.observable();
+
+    this.show_warning = ko.observable(false);
+    this.provider.subscribe(
+      (value) => {
+        if (this.provider_original === undefined) {
+          this.provider_original = value;
+        }
+      },
+      this,
+      "beforeChange",
+    );
+    this.provider.subscribe((value) => {
+      if (
+        this.provider_original !== undefined &&
+        value !== this.provider_original
+      ) {
+        this.show_warning(true);
+      } else {
+        this.show_warning(false);
+      }
+    });
+    this.use_domain = ko.computed(() => {
+      return this.provider() === "email";
+    });
+  }
+}
+Registry.add_view(OrganizationSettingsAuthorizationView);
