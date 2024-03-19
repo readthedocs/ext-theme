8f77af564e8000bf79b89c4e2a896b835d7575cc
Anthony Johnson <aj@ohess.org>
2024-03-19 09:45:14 -0700

    WIP, includes web component for notification

--- src/js/modules/notifications.js
+++ src/js/modules/notifications.js
@@ -1,18 +1,92 @@
 import jquery from "jquery";
 import { LitElement, css, html, nothing } from "lit";
-import {repeat} from 'lit/directives/repeat.js';
-import {when} from 'lit/directives/when.js';
-import {classMap} from 'lit/directives/class-map.js';
-import {unsafeHTML} from 'lit/directives/unsafe-html.js';
-import {ref, createRef} from 'lit/directives/ref.js';
+import { repeat } from "lit/directives/repeat.js";
+import { when } from "lit/directives/when.js";
+import { classMap } from "lit/directives/class-map.js";
+import { unsafeHTML } from "lit/directives/unsafe-html.js";
+
+/* Notification
+ *
+ * Used internally and directly to render a notification API response. This
+ * element is not rendered using element attributes.
+ */
+export class Notification extends LitElement {
+  static properties = {
+    csrfToken: { type: String, attribute: "csrf-token" },
+    notification: { state: true },
+  };
+
+  // Use light DOM with inherited styles instead of shadow DOM
+  createRenderRoot() {
+    return this;
+  }
+
+  render() {
+    if (this.notification === undefined) {
+      return nothing;
+    }
+    return html`
+      <div class="ui small ${this.notification.message.type} message">
+        ${when(
+          this.notification.dismissable,
+          () => html`
+            <i
+              class="fas fa-xmark close inline icon"
+              @click=${this.dismiss}
+            ></i>
+          `,
+        )}
+        <h5 class="header">
+          <i class="fad ${this.notification.message.icon_classes} icon"></i>
+          ${unsafeHTML(this.notification.message.header)}
+        </h5>
+        ${unsafeHTML(this.notification.message.body)}
+      </div>
+    `;
+  }
+
+  dismiss() {
+    console.debug("Dismissing notification:", this.notification.id);
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": this.csrfToken,
+      },
+      body: JSON.stringify({
+        state: "dismissed",
+      }),
+    };
+    fetch(this.notification._links._self, options).then((response) => {
+      if (response.ok) {
+        // Use FUI transition module to fade out and remove the notification
+        jquery(this).transition({
+          animation: "fade",
+          onComplete: () => {
+            this.parentElement.removeChild(this);
+          },
+        });
+      } else {
+        console.debug("Error dismissing notification", response);
+      }
+    });
+  }
+}
 
 export class NotificationList extends LitElement {
   static properties = {
-    url: {type: String},
-    csrfToken: {type: String, attribute: "csrf-token"},
-    notifications: {state: true},
-    finished: {state: true, type: Boolean},
+    url: { type: String },
+    csrfToken: { type: String, attribute: "csrf-token" },
+    state: { type: String },
+    notifications: { state: true },
+    finished: { state: true, type: Boolean },
+    build: { type: String },
   };
+
+  constructor() {
+    super();
+    this.state = "unread";
+  }
 
   // Use light DOM with inherited styles instead of shadow DOM
   createRenderRoot() {
@@ -23,7 +97,10 @@ export class NotificationList extends LitElement {
     if (!this.url || this.finished) {
       return;
     }
-    fetch(this.url).then((response) => {
+    const params = new URLSearchParams({
+      state: this.state,
+    });
+    fetch(`${this.url}?${params}`).then((response) => {
       response.json().then((data) => {
         if (data.results) {
           this.notifications = data.results;
@@ -38,70 +115,28 @@ export class NotificationList extends LitElement {
     this.fetchNotifications();
 
     if (this.notifications && this.notifications.length > 0) {
-      return html`
-        <div class="ui vertical relaxed list">
-          ${repeat(
-            this.notifications,
-            (notification) => notification.id,
-            (notification, index) => {
-              return this.renderNotification(notification);
-            }
-          )}
-        </div>
-      `;
+      //return html`
+      //    ${repeat(
+      return repeat(
+        this.notifications,
+        (notification) => notification.id,
+        (notification, index) => {
+          const elem = document.createElement("readthedocs-notification");
+          elem.notification = notification;
+          elem.csrfToken = this.csrfToken;
+
+          // For FUI .ui.list.item
+          elem.className = "item";
+          return elem;
+        },
+      );
+      //    )}
+      //`;
     } else {
       return nothing;
     }
   }
-
-  renderNotification(notification) {
-    if (notification.dismissed) {
-      return nothing;
-    }
-
-    // Direct reference to 
-    const refClose = createRef();
-
-    return html`
-      <div class="ui small ${notification.message.type} message" ${ref(refClose)}>
-        ${when(notification.dismissable, () => html`
-          <i class="fas fa-xmark close inline icon" @click=${{handleEvent: () => this.dismissNotification(notification, refClose.value), once: true}}></i>
-        `)}
-        <h5 class="header">
-          <i class="fad ${notification.message.icon_classes} icon"></i>
-          ${unsafeHTML(notification.message.header)}
-        </h5>
-        ${unsafeHTML(notification.message.body)}
-      </div>
-    `
-  }
-
-  dismissNotification(notification, element) {
-    if (notification.dismissable === false) {
-      return;
-    }
-
-    console.debug("Dismissing notification:", notification.id);
-    const options = {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRFToken": this.csrfToken,
-      },
-      body: JSON.stringify({
-        state: "dismissed",
-      }),
-    };
-    fetch(notification._links._self, options).then((response) => {
-      if (response.ok) {
-        // Use FUI transition module to fade out and remove the notification
-        jquery(element).transition("fade");
-      }
-      else {
-        console.debug("Error dismissing notification", response);
-      }
-    });
-  }
 }
 
-customElements.define("rtd-notification-list", NotificationList);
+customElements.define("readthedocs-notification", Notification);
+customElements.define("readthedocs-notification-list", NotificationList);
