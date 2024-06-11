757f31ba65a6395f9e8e7b07ac6aba63f04c20b0
Manuel Kaufmann <humitos@gmail.com>
2024-06-11 13:17:53 +0200

    Use a `.ui.link.list` for this pattern

--- src/js/organization/index.js
+++ src/js/organization/index.js
@@ -39,3 +39,13 @@ export class OrganizationSettingsAuthorizationView {
   }
 }
 Registry.add_view(OrganizationSettingsAuthorizationView);
+
+export class OrganizationCreateView {
+  static view_name = "OrganizationCreateView";
+
+  constructor() {
+    // Enabling tab switching on a list
+    $(".list .item").tab();
+  }
+}
+Registry.add_view(OrganizationCreateView);
