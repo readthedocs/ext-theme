f0e2de54e89b15968bf61d5d05ea0fef8d51df11
Anthony Johnson <aj@ohess.org>
2024-11-22 15:47:40 -0800

    Add base for context data elements

--- src/js/modules/menus.js
+++ src/js/modules/menus.js
@@ -1,5 +1,9 @@
 import jquery from "jquery";
 import { LitElement, css, html, nothing } from "lit";
+import { ContextProvider, ContextConsumer, createContext } from "@lit/context";
+import { msg } from "@lit/localize";
+import { classMap } from "lit/directives/class-map.js";
+import { when } from "lit/directives/when.js";
 
 import { LightDOMElement } from "../application/elements";
 
@@ -145,3 +149,159 @@ customElements.define(
   "readthedocs-button-sync-versions",
   ButtonSyncAllVersionsElement,
 );
+
+/**
+ * Lit data contexts for elements with API interactions
+ *
+ * These data contexts are provided by :js:class:`APIProviderElement` and
+ * consumed by :js:class:`APIConsumerElement`. This allows a central element to
+ * execute an API request (table row, or a group of buttons) and for sub elements
+ * at any point in the nested DOM to use this response (a button nested in the
+ * table row, a submenu in the group of buttons).
+ **/
+const contextData = createContext(Symbol("data"));
+const contextState = createContext(Symbol("state"));
+
+/**
+ * API resopnse states enum
+ *
+ * These are used by API response provider/consumer
+ **/
+const States = Object.freeze({
+  WAITING: Symbol("waiting"),
+  LOADING: Symbol("loading"),
+  DONE: Symbol("done"),
+});
+
+/**
+ * Context provider element for API response
+ *
+ * On an event (mouseover by default), this element starts an API request to the
+ * URL specified. It eventually stores the response data in a data context for
+ * nested consumer elements.
+ *
+ * Handles error feedback via toast message and redirection on success.
+ *
+ * @extends APIEventWrapper
+ *
+ * TODO This can replace the parent APIEventWrapper element above
+ **/
+export class APIProviderElement extends APIEventWrapper {
+  static method = "GET";
+  static event = "mouseover";
+
+  /** @type {ContextProvider} Reactive data context for API response data **/
+  _providerData = new ContextProvider(this, {
+    context: contextData,
+  });
+
+  set data(value) {
+    this._providerData.setValue(value);
+  }
+
+  /** @type {ContextProvider} Reactive data context for API response state **/
+  _providerState = new ContextProvider(this, {
+    context: contextState,
+  });
+
+  set state(value) {
+    this._providerState.setValue(value);
+  }
+
+  constructor() {
+    super();
+    this.state = States.WAITING;
+  }
+
+  onEvent() {
+    if (this.request == undefined) {
+      this.state = States.LOADING;
+      this.sendRequest().finally(() => {
+        this.state = States.DONE;
+      });
+    }
+  }
+
+  onResponse(data) {
+    this.data = data;
+  }
+}
+
+customElements.define("readthedocs-api", APIProviderElement);
+
+/**
+ * Context consumer element for API response
+ *
+ * This element waits for a parent anywhere up the DOM tree to emit a data
+ * context element. There are two data contexts: the response state and the
+ * response data.
+ *
+ * @extends LightDOMElement
+ *
+ * @property {Boolean} disabled - Render the element in a disabled state
+ * @property {String} label - Label for the element and ARIA
+ **/
+export class APIConsumerElement extends LightDOMElement {
+  static properties = {
+    disabled: {
+      type: Boolean,
+    },
+    label: {
+      type: String,
+    },
+  };
+
+  /** @type {ContextConsumer} Reactive consumer for API response context data **/
+  _consumerData = new ContextConsumer(this, {
+    context: contextData,
+    subscribe: true,
+  });
+
+  get data() {
+    return this._consumerData.value;
+  }
+
+  /** @type {ContextConsumer} Reactive consumer for API request state context data **/
+  _consumerState = new ContextConsumer(this, {
+    context: contextState,
+    subscribe: true,
+  });
+
+  /** @type {State} Reactive context data for API request state **/
+  get state() {
+    return this._consumerState.value;
+  }
+
+  constructor() {
+    super();
+    this.disabled = false;
+  }
+
+  /**
+   * Queue event for late loading links
+   *
+   * Wait until the request promise resolves and then reemit the event to
+   * finally follow the link. This is useful for links that have a late-loaded
+   * ``href``.
+   *
+   * Use it in templates with ``html`<div @click="${this.queueEvent}">``
+   *
+   * @param event {Event} - Click or other event
+   **/
+  queueEvent(event) {
+    if (this.state !== States.DONE) {
+      console.debug("Queueing click event:", event);
+      event.preventDefault();
+      event.stopPropagation();
+
+      this._consumerState.callback = (value) => {
+        if (value === States.DONE) {
+          console.debug("Replaying click event:", event);
+          // dispatchEvent doesn't seem to work here, so we just create a brand
+          // new event here instead.
+          event.target.click();
+        }
+      };
+    }
+  }
+}
