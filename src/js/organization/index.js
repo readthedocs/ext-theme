7c2b6d666188d47832e916fd21320028e5ed1e79
Manuel Kaufmann <humitos@gmail.com>
2025-07-07 11:15:38 +0200

    Use knockout pattern and show UI with example

--- src/js/organization/index.js
+++ src/js/organization/index.js
@@ -47,26 +47,36 @@ export class OrganizationCreateView {
   static view_name = "OrganizationCreateView";
 
   constructor() {
-    const nameInput = document.querySelector("input[name=name");
-    const slugInput = document.querySelector("input[name=slug]");
-
-    const slugify = (val) => {
-      return (
-        val
-          .toString()
-          .toLowerCase()
-          .trim()
-          // Replace spaces, non-word chars, underscores and dashes with a single '-'
-          // Copied from Django, which is what we are using under the hood
-          // https://github.com/django/django/blob/1e9db35/django/utils/text.py#L469-L470
-          .replace(/[^\w\s-_]+/g, "-")
-          .replace(/[-\s]+/g, "-")
-      );
-    };
-
-    nameInput.addEventListener("keyup", (e) => {
-      slugInput.value = slugify(nameInput.value);
+    this.name = ko.observable();
+    this.slug = ko.observable();
+    this.name.subscribe((name) => {
+      const slugified = this.slugify(name);
+      this.slug(slugified);
     });
+    this.example = ko.computed(() => {
+      const slug = this.slug();
+      if (!slug) {
+        return "";
+      }
+      const example =
+        "https://" + slug + "-example-project.readthedocs-hosted.com";
+      return example;
+    });
+  }
+
+  slugify(val) {
+    if (!val) return "";
+    return (
+      val
+        .toString()
+        .toLowerCase()
+        .trim()
+        // Replace spaces, non-word chars, underscores and dashes with a single '-'
+        // Copied from Django, which is what we are using under the hood
+        // https://github.com/django/django/blob/1e9db35/django/utils/text.py#L469-L470
+        .replace(/[^\w\s-_]+/g, "-")
+        .replace(/[-\s]+/g, "-")
+    );
   }
 }
 Registry.add_view(OrganizationCreateView);
