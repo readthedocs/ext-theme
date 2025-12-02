5b444ff44cf925a1ccd7bed735fbf847809f064a
Anthony Johnson <aj@ohess.org>
2025-12-01 23:04:14 -0800

    Some cleanup

--- src/js/account/index.js
+++ src/js/account/index.js
@@ -40,7 +40,7 @@ export class SocialAccountView {
  *
  *   <div data-bind="using: LoginView()">
  *     <form method="post" action="...">
- *       <button data-bind="click: save_login_method.bind('email'), semanticui: { popup: popup_login_method('email') }">
+ *       <button data-bind="click: save_login_method.bind($data, 'email'), semanticui: { popup: popup_login_method('email') }">
  *         Log in using GitHub
  *       </button>
  *     </form>
@@ -55,8 +55,7 @@ export class LoginView extends SocialAccountView {
     /** @observable {string} Last tab to be selected. Comes from view */
     this.last_tab = ko.observable(options?.last_tab || "vcs");
     /** @observable {string} Last method to be used. Comes from view */
-    // XXX fix default, this is just for testing
-    this.last_method = ko.observable(options?.last_method || "githubapp");
+    this.last_method = ko.observable(options?.last_method);
 
     // This is an named lookup for observables, one for each method. It is
     // populated by the template code as each popup is configured.
@@ -76,12 +75,11 @@ export class LoginView extends SocialAccountView {
    * @param {string} position - Position to pass to SUI popup position attribute
    * @param {string} method - Method id
    */
-  popup_login_method(method, position = "top center", fuzzy = true) {
+  popup_login_method(method, position = "top center") {
     this.popups[method] = ko.observable((popup) => {
-      // This is using the KO data binding method for utilizing SUI module
-      // behaviors. Passing this an anonymous function provides access to
-      // `$(element).popup()` essentially.
       // First set up the element as a manual popup, then manually show it.
+      // This is using the anonymous function pattern for passing arguments to
+      // the ``popup()`` jQuery plugin binding, via :func:`application.plugins.semanticui`
       popup({
         content: msg(`Last used`),
         position: position,
@@ -107,7 +105,7 @@ export class LoginView extends SocialAccountView {
    * .. code:: html
    *
    *     <form method="post" action="...">
-   *       <button data-bind="click: save_login_method.bind('email')">
+   *       <button data-bind="click: save_login_method.bind($data, 'email')">
    *         Log in using GitHub
    *       </button>
    *     </form>
@@ -132,7 +130,6 @@ export class LoginView extends SocialAccountView {
     super.show_github_modal();
     const last_method = this.last_method();
     if (["github", "githubapp"].includes(last_method)) {
-      console.debug(last_method, this.popups, this.popups[last_method]);
       this.popups[last_method]((popup) => popup("show"));
     }
   }
