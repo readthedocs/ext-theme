d855729a26a9a138f5fdbd3b67d01fa6094524f3
Anthony Johnson <aj@ohess.org>
2026-07-08 23:40:51 -0700

    Make pre-commit config skip build asset detection if tree is dirty

--- .pre-commit/check-build-assets.sh
+++ .pre-commit/check-build-assets.sh
@@ -1,9 +1,26 @@
-#!/bin/bash
+#!/bin/sh
+
+# If the working tree already has unstaged changes from earlier
+# auto-fix hooks, don't both running this check and just report fail
+if tree_before=$(git status --porcelain); then
+    cat <<EOF
+Working tree is already dirty before asset build.
+
+% git status --porcelain
+${tree_before}
+EOF
+    exit 1
+fi
 
 npm run build
 
-if [[ `git diff readthedocsext/` ]]
+if tree_after=$(git diff readthedocsext/); then
 then
-    echo "ERROR: assets are out of date. Make sure to run 'npm run build' on your branch."
+    cat <<EOF
+Assets are out of date. Make sure to run 'npm run build' on your branch.
+
+% git diff readthedocsext/ 
+${tree_after}
+EOF
     exit 1
 fi
